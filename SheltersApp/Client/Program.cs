d1f90c46e8459bf21febce2df62ac89dd9993e52
rosell <Gustavrosell@gmail.com>
2023-11-06 14:55:58 +0100

    hehe siu

--- SheltersApp/Client/Program.cs
+++ SheltersApp/Client/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SheltersApp.Client;
-using SheltersApp.Shared.Storage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
